cb5387b6df60b5b3635b757ee63923dec29402a7
kkilcommons <70309683+kkilcommons@users.noreply.github.com>
2020-12-03 16:38:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,22 @@
 
 import pyvisa as visa
 import pwr01
+import time
 
-rm = visa.ResourceManager('@py')
+rm = visa.ResourceManager()
 print(rm.list_resources())
 
-instrument = rm.open_resource('COM2')
+instrument = rm.open_resource('USB0::0x0B3E::0x1049::XE001118::INSTR')
 
-PWR_01_A = pwr01.Pwr_01(instrument, '123456', 'PWR401ML')
+PWR_01_A = pwr01.Pwr_01(instrument)
+# print(instrument.query('*IDN?'))
+# instrument.write('VOLT 40')
+# print(instrument.query('VOLT ?'))
+
 
 PWR_01_A.set_voltage(40)
+print(PWR_01_A.read_voltage())
 
-
+PWR_01_A.output(1)
+time.sleep(7)
+PWR_01_A.output(0)
